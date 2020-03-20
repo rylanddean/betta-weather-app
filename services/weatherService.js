848f440c9c21bb5fecc36a61af280bd9f11304f6
rylanddean <ryland.w.dean@gmail.com>
2020-03-19 20:13:20 -0400

    Need to find a way to console log

--- services/weatherService.js
+++ services/weatherService.js
@@ -3,9 +3,9 @@ function getWeatherData()
     const fetchPromise = fetch('https://api.darksky.net/forecast/d8a9f6d83305c8cc16d85b67aa65dae9/37.8267,-122.4233');
     fetchPromise.then(response => {
         alert(response);
+        return response.json();
+    }).then(people => {
+        const names = people.map(person => person.name).join("\n");
+        alert(names);
     });
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", 'https://api.darksky.net/forecast/d8a9f6d83305c8cc16d85b67aa65dae9/37.8267,-122.4233', false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
 }
